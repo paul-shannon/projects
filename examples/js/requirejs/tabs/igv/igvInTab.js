305e74b7b528a4c727c52a7dfddb809162f381e8
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-01-22 12:01:44 -0800

    now uses apache locally-served data: gencode, searchable geneSymbols, igap gwas

--- examples/js/requirejs/tabs/igv/igvInTab.js
+++ examples/js/requirejs/tabs/igv/igvInTab.js
@@ -38,13 +38,48 @@ require(['igv'], function (igv) {
                 }
             ]
         }; // options
+
+
+    newOptions = {locus: "5:88,621,548-88,999,827",
+            reference: {id: "geneSymbols_hg38",
+                fastaURL: "http://localhost/data/genomes/human_g1k_v37_decoy.fasta",
+                cytobandURL: "http://localhost/data/annotations/b37_cytoband.txt"
+                },
+               tracks: [
+                 {name: 'Gencode v24',
+                  url: "http://localhost://data/hg38/gencode.v24.annotation.sorted.gtf.gz",
+                  indexURL: "http://localhost://data/hg38/gencode.v24.annotation.sorted.gtf.gz.tbi",
+                  format: 'gtf',
+                  visibilityWindow: 1000000,
+                  displayMode: 'EXPANDED'
+                  },
+                 {name: 'geneSymbols_hg38',
+                  url: 'http://localhost/data/hg38/geneSymbolSearch.bed',
+                  indexed: false,
+                  searchable: true,
+                  //visibilityWindow: 5000000,
+                  displayMode: 'COLLAPSED',
+                  color: "#448844"
+                  },
+                 {name: "igap gwas",
+                  url: 'http://localhost/data/hg38/variants/igap.bed',
+                  indexed: false,
+                  searchable: true,
+                  //visibilityWindow: 5000000,
+                  displayMode: 'EXPANDED',
+                  color: "#884444"
+                  }
+                 
+                 ]
+              }; // newOptions
+
         $("#tabs").tabs({
            create: function(event, ui){console.log(tabs.create)}
            });
         console.log("about to createBrowser");
         setTimeout(function(){
            $("#igvDiv").text(""); // remove any message
-           browser = igv.createBrowser(igvDiv, options);
+           browser = igv.createBrowser(igvDiv, newOptions);
            }, 5000);
     }); // onready funcetion
 });
