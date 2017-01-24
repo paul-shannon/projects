f211c06d294d6a1606639423bfe4a2fce69b3c61
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-01-24 15:50:06 -0800

    added for loop

--- examples/rpc/zmq/request-reply/client.py
+++ examples/rpc/zmq/request-reply/client.py
@@ -3,6 +3,7 @@ socketContext = zmq.Context()
 socket = socketContext.socket(zmq.REQ)
 socket.connect("tcp://localhost:%s" % '5556')
 
-socket.send_string("hello from python")
-print(socket.recv_string())
+for i in range(5):
+   socket.send_string("hello from python %d" % i)
+   print(socket.recv_string())
 
