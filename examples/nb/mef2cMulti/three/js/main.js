fc815bfa5bacb73f3f6e70738e9b1bd1ffb2a4c2
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-02-05 13:08:32 -0800

    separate three module now used in notebook

---
+++ examples/nb/mef2cMulti/three/js/main.js
@@ -0,0 +1,10 @@
+require(['app3d'], function(app3d){
+
+   window.app3d = app3d;
+   app3d.init("threeDiv");
+   var data = [{"chr":6,"pos":147596,"an":1,"af":0.9812,"EC1":0.0244},
+               {"chr":6,"pos":148039,"an":1,"af":0.7887,"EC1":-0.1367}];
+   app3d.drawScatterPlot(data);
+   app3d.animate()
+
+   });
