711938208b7038cad1a93c93d405740e898622ad
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-04-24 14:48:09 -0700

    buttons work

--- examples/js/webpack/learning-webpack2/moreModularCyjs/src/entry.js
+++ examples/js/webpack/learning-webpack2/moreModularCyjs/src/entry.js
@@ -4,13 +4,33 @@ $("#cyDiv").height(1000);
 var bootstrapLoader = require('bootstrap-loader');
 var network = require("./data/cellphoneModel.json")
 var vizmap  = require("./data/networkStyle.json")
+var tinyApp = require("./tinyApp.js")
+var firstSelectedNode = null;
+
 var cy = cytoscape({container: $("#cyDiv"),
                     elements: network.elements,
                     layout: {name: "preset", fit: true},
-                    style: vizmap});
-var tinyApp = require("./tinyApp.js")
+                    style: vizmap,
+                    ready: function(){
+                      cy = this;
+                      cy.on("select", function(event){
+                         if(cy.nodes(":selected").length == 1){
+                            firstSelectedNode = event.target.id();
+                            }
+                         tinyApp.enableDisableMenusBasedOnSelectedNodeCount(cy);
+                         });
+                     cy.on("unselect", function(){
+                       tinyApp.enableDisableMenusBasedOnSelectedNodeCount(cy);
+                       });
+                    console.log("small cy network ready, with " + cy.nodes().length + " nodes.");
+                   cy.fit(50);
+                   } // ready
+               }); // cytoscape ctor
+
 tinyApp.init()
 $(window).resize(tinyApp.handleWindowResize);
+tinyApp.handleWindowResize();
+tinyApp.setupMenus(cy);
 window.cy = cy;
 
 
--- examples/js/webpack/learning-webpack2/moreModularCyjs/src/tinyApp.js
+++ examples/js/webpack/learning-webpack2/moreModularCyjs/src/tinyApp.js
@@ -1,26 +1,119 @@
 // tinyApp.js
 
+var sfnButton = null;
+
 module.exports = {
+
 
     init: function(){
        console.log("hello fromm tinyApp.init");
        },
 
     handleWindowResize: function(){
-        console.log("tinyApp.handleWindowResize");
-        console.log("=== window: ")
-        console.log($(window));
-        console.log("window height: ");
-        console.log($(window).height());
         var outermostDivHeight = $("#outermostDiv").height();
-        console.log("outermostDiv height: ");
-        console.log(outermostDivHeight);
         var menubarHeight = $("#menubarDiv").height();
         var newHeight = outermostDivHeight - (menubarHeight + 20);
-
-        console.log("==== newHeight: " + newHeight);
         $("#cyDiv").height(newHeight)
-        }
+        },
 
-}
+    selectShortestPath: function() {
+       var selectedNodes = cy.nodes(":selected");
+       if(selectedNodes.length != 2){
+          return;
+          }
+
+        var selectedNodesIDs = selectedNodes.map(function(obj){return obj.id()});
+        var rootNode = window.singleSelectedNode;
+        var rootNodeID = rootNode[0].id();
+        var rootNodeIndex = selectedNodesIDs.indexOf(rootNodeID);
+        var targetNodeIndex = 1;
+        if(rootNodeIndex == 1){
+           targetNodeIndex = 0;  // just two possibilities, 0 or 1
+           }
+        var targetNode = selectedNodes[targetNodeIndex]
+        var d = cy.elements().dijkstra({root: rootNode, directed: true});
+        var path = d.pathTo(targetNode)
+        var nodesInPath = path.nodes();
+        nodesInPath.select()
+        var max = nodesInPath.length - 1;
+        for(var i=0; i < max; i++){
+            nodesInPath[i].edgesTo(nodesInPath[i+1]).select();
+           } // for i
+        }, // selectShortestPath
+
+    setupMenus: function(cy){
+       knockoutButton = $("#knockoutButton");
+       knockoutButton.prop('disabled', true);
+       knockoutButton.click(function(){});
+
+       restoreButton = $("#restoreButton");
+       restoreButton.prop('disabled', true);
+       restoreButton.click(function(){});
+
+       phoneTreeButton = $("#phoneTreeButton");
+       phoneTreeButton.prop('disabled', true);
+       phoneTreeButton.click(function(){});
+
+       shortestPathButton = $("#shortestPathButton");
+       shortestPathButton.prop('disabled', false);
+       shortestPathButton.click(this.selectShortestPath);
+
+       resetGraphButton = $("#resetGraphButton");
+       resetGraphButton.prop('disabled', true);
+       resetGraphButton.click(function(){});
+
+       helpButton = $("#helpButton");
+       helpButton.click(function(){});
+
+       fitButton = $("#fitButton");
+       fitButton.click(function(){cy.fit(50)});
+
+       fitSelectedButton = $("#fitSelectedButton");
+       fitSelectedButton.prop('disabled', false);
+
+       fitSelectedButton.click(function(){
+         var selectedNodes = cy.filter('node:selected');
+         if(selectedNodes.length > 0){
+           cy.fit(selectedNodes, 50)
+           }});
+
+       clearSelectionsButton = $("#clearSelectionsButton");
+       clearSelectionsButton.prop('disabled', true);
+       clearSelectionsButton.click(function(){
+          cy.nodes(":selected").unselect()
+          cy.edges(":selected").unselect()
+          });
+
+       sfnButton = $("#sfnButton");
+       sfnButton.prop('disabled', true);
+       sfnButton.click(function(){cy.nodes(":selected").outgoers().targets().select()});
+       }, // setupMenus
+
+    enableDisableMenusBasedOnSelectedNodeCount: function(cy){
+       var selectedNodeCount = cy.nodes(":selected").length
+        console.log("=== entering enableDisableMenusBasedOnSelectedNodeCount: " + selectedNodeCount);
+        if(selectedNodeCount > 0){
+          clearSelectionsButton.prop('disabled', false);
+          sfnButton.prop("disabled", false);
+          }
+       switch(selectedNodeCount){
+          case 0:
+             window.singleSelectedNode = null;
+             sfnButton.prop("disabled", true);
+             break;
+          case 1:
+             window.singleSelectedNode = cy.nodes(":selected").map(function(obj){return obj});
+             sfnButton.prop("disabled", false);
+             break;
+          case 2:
+             // turn on the "shortest path" button
+             break;
+          default:  // > 2 nodes selected
+             window.singleSelectedNode = null;
+           } // switch on selectedNodeCount
+       } // enableDisableMenusBasedOnSelectedNodeCount
+
+
+
+} // module.exports
 
