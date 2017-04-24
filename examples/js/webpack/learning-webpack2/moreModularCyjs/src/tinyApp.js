5c689fe72f8f010c8587f94213a1040be3b733f2
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-04-24 13:31:14 -0700

    with outerdiv in css, resize now works properly

--- examples/js/webpack/learning-webpack2/moreModularCyjs/src/tinyApp.js
+++ examples/js/webpack/learning-webpack2/moreModularCyjs/src/tinyApp.js
@@ -8,6 +8,18 @@ module.exports = {
 
     handleWindowResize: function(){
         console.log("tinyApp.handleWindowResize");
+        console.log("=== window: ")
+        console.log($(window));
+        console.log("window height: ");
+        console.log($(window).height());
+        var outermostDivHeight = $("#outermostDiv").height();
+        console.log("outermostDiv height: ");
+        console.log(outermostDivHeight);
+        var menubarHeight = $("#menubarDiv").height();
+        var newHeight = outermostDivHeight - (menubarHeight + 20);
+
+        console.log("==== newHeight: " + newHeight);
+        $("#cyDiv").height(newHeight)
         }
 
 }
