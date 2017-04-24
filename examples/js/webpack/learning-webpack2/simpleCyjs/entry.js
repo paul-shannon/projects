88b1d806073c711b609c38365472249aa2a47456
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-04-23 17:24:57 -0700

    displays all nodes, proper style

--- examples/js/webpack/learning-webpack2/simpleCyjs/entry.js
+++ examples/js/webpack/learning-webpack2/simpleCyjs/entry.js
@@ -5,6 +5,8 @@ console.log(cytoscape);
 $("#cyDiv").height(300);
 var network = require("./cellphoneModel.json")
 var vizmap  = require("./networkStyle.json")
-var cy = cytoscape({container: $("#cyDiv"), elements:{nodes:[{data:{id:'a'}}], edges:[{data:{source:'a', target:'a'}}]}})
+//var cy = cytoscape({container: $("#cyDiv"), elements:{nodes:[{data:{id:'a'}}], edges:[{data:{source:'a', target:'a'}}]}})
+var cy = cytoscape({container: $("#cyDiv"), elements: network.elements, style: vizmap});
+window.cy = cy;
 
 
