2fbc89e427deb6ce189b586e9bc68dd60c557051
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-04-24 11:54:47 -0700

    jquery & bootstrap installed, no button callbacks or resize working yet

--- examples/js/webpack/learning-webpack2/moreModularCyjs/src/entry.js
+++ examples/js/webpack/learning-webpack2/moreModularCyjs/src/entry.js
@@ -1,7 +1,7 @@
 import css from './css/style.css';
-document.write(require("./content.js"));
 var cytoscape = require('cytoscape');
 $("#cyDiv").height(1000);
+var bootstrapLoader = require('bootstrap-loader');
 var network = require("./data/cellphoneModel.json")
 var vizmap  = require("./data/networkStyle.json")
 var cy = cytoscape({container: $("#cyDiv"),
--- examples/js/webpack/learning-webpack2/moreModularCyjs/webpack.config.js
+++ examples/js/webpack/learning-webpack2/moreModularCyjs/webpack.config.js
@@ -12,11 +12,20 @@ module.exports = {
   module: {
     rules:[{
        test: /\.css$/,
-       use: [ 'style-loader', 'css-loader' ]
-       }]
-    },
+        use: [ 'style-loader', 'css-loader']
+       },
+      {test: /\.png$/,
+       loader: 'url-loader?limit=100000'
+       },
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+       loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+       },
+      {test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+       loader: 'file-loader'
+       }
+          ]},
   output: {
    path: path.resolve(__dirname, 'dist'),
    filename: 'bundle.js'
-   },
+   }
 };
