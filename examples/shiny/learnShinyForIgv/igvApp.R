733af57ec0ccd2ef9ef6860ca2035314626f3ed8
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-03-13 08:43:59 -0700

    sidebarLayout, action button

--- examples/shiny/learnShinyForIgv/igvApp.R
+++ examples/shiny/learnShinyForIgv/igvApp.R
@@ -2,29 +2,43 @@ library(shiny)
 library(igv)
 library(htmlwidgets)
 
-igvOutput <- function(outputId, width = "100%", height = "400px") {
+#----------------------------------------------------------------------------------------------------
+igvOutput <- function(outputId, width = "100%", height = "400px")
+{
   shinyWidgetOutput(outputId, "igv", width, height, package = "igv")
 }
-
-renderIgv <- function(expr, env = parent.frame(), quoted = FALSE) {
+#----------------------------------------------------------------------------------------------------
+renderIgv <- function(expr, env = parent.frame(), quoted = FALSE)
+{
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, igvOutput, env, quoted = TRUE)
 }
-
+#----------------------------------------------------------------------------------------------------
 ui = shinyUI(fluidPage(
   tags$head(
           tags$link(rel = "stylesheet", type = "text/css",
                     href = "http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css")
     ),
-  checkboxInput("drawEdges", "Draw Edges", value = TRUE),
-  checkboxInput("drawNodes", "Draw Nodes", value = TRUE),
-  igvOutput('igv')
+  sidebarLayout(
+     sidebarPanel(
+        actionButton("action", label="Action"),
+        hr(),
+        checkboxInput("drawEdges", "Draw Edges", value = TRUE),
+        checkboxInput("drawNodes", "Draw Nodes", value = TRUE),
+        width=2
+        ),
+     mainPanel(
+        igvOutput('igv'),
+        width=10
+       )
+     ) # sidebarLayout
 ))
-
+#----------------------------------------------------------------------------------------------------
 server = function(input, output) {
+  output$value <- renderPrint({ input$action })
   output$igv <- renderIgv(
     igv("hello shinyApp")
   )
 }
-
+#----------------------------------------------------------------------------------------------------
 shinyApp(ui = ui, server = server)
